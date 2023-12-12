f1f449a7a1e1bc31c4d1cdaa057fda2be84d97bb
TonyStranger404 <53440318+TonyStranger404@users.noreply.github.com>
2023-12-13 00:21:02 +0500

    Update Salov.py

--- Salov.py
+++ Salov.py
@@ -1,14 +1,5 @@
-import torch
-from transformers import AutoModelForSequenceClassification
-from transformers import BertTokenizerFast
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-tokenizer = BertTokenizerFast.from_pretrained('blanchefort/rubert-base-cased-sentiment')
-model = AutoModelForSequenceClassification.from_pretrained('blanchefort/rubert-base-cased-sentiment', return_dict=True)
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-ru-en")
 
-
-def predict(text):
-    inputs = tokenizer(text, max_length=512, padding=True, truncation=True, return_tensors='pt')
-    outputs = model(**inputs)
-    predicted = torch.nn.functional.softmax(outputs.logits, dim=1)
-    predicted = torch.argmax(predicted, dim=1).numpy()
-    return predicted
+model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-ru-en")
