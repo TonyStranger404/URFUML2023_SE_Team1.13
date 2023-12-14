73385c8f46339c25dc4079e754400db493b9ea1c
Anton Salov <ahakohda1001@gmail.com>
2023-12-15 02:29:11 +0500

    add test script

---
+++ Salov_AS/test_salov.py
@@ -0,0 +1,22 @@
+from fastapi.testclient import TestClient
+from Salov_HW3_FastApi import app
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "programm start, input your text"}
+
+def test__translate_first():
+    response = client.post("/predict/", json={"text": "Как у тебя дела?"})
+    json_data = response.json()
+    #print(json_data)
+    assert response.status_code == 200
+    assert json_data['translation_text'] == 'How are you doing?'
+
+def test_translate_second():
+    response = client.post("/predict/", json={"text": "Проверка перевода"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data['translation_text'] == "Translation check"
